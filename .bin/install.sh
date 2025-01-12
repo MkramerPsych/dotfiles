8128f7a2f8dcbb402267ab2ed116b66135993fd2
Max Kramer <max@realitydefender.ai>
2025-01-12 17:10:21 -0600

    adjust install.sh for subdirectories

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,14 +1,20 @@
 git clone --bare git@github.com:MkramerPsych/dotfiles.git $HOME/.dots
+
 function config {
    /usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME $@
 }
+
 mkdir -p .dots-backup
+
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dots-backup/{}
-fi;
+else
+  echo "Backing up pre-existing dot files.";
+  config checkout 2>&1 | egrep "\s+\." | awk '{print $1}' | \
+  xargs -I{} sh -c 'mkdir -p $(dirname .dots-backup/{}) && mv {} .dots-backup/{}'
+fi
+
 config checkout
 config config status.showUntrackedFiles no
+
